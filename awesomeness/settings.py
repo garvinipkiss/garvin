62ca49af42a6c114eddd05204a8e983cbacc0919
garvinipkiss <garvinipkiss@gmail.com>
2018-05-08 15:41:26 +0300

    final commit

--- awesomeness/settings.py
+++ awesomeness/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -87,6 +86,8 @@ DATABASES = {
         'PASSWORD': '0000',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
